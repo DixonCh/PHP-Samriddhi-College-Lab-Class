3ea59dbb3236f659a7afa1d00c5c127581806cd8
DixonCh <dixonchaudhary7@gmail.com>
2020-04-16 13:28:59 +0545

    Update concatenation example.php

--- concatenation example.php
+++ concatenation example.php
@@ -1,9 +1,8 @@
+//Program to show the example of concatenation
 <?php
 $statement='everything I say'; 
-//$question_1="Do you have to take $statement so literally?"\n<BR>;
-//$question_2="Do you have to take $statement so literally?"\n<BR>;
+$question_1="Do you have to take $statement so literally?"\n<BR>;
 echo $question_1;
-echo $question_2;
 $sport1='volley';
 $sport2='foot';
 $paln3="I will play $sport1ball in the fall"; //wrong
@@ -11,5 +10,4 @@ $plan2="I will play $sport2ball in the fall";
 $plan1="I will play {$sport1}ball in the summertime"; //right
 $short_string="This string has 29 charcters";
 print("It does have ".strlen($short_string)."characters");
-
 ?>
